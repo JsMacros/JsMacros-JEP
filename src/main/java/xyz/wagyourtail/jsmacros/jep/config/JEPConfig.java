59946359be734dfb0b4e178fc12e53a61d63d350
wagyourtail <wagyourtail@wagyourtail.xyz>
2021-04-06 16:06:10 -0700

    add translations

--- src/main/java/xyz/wagyourtail/jsmacros/jep/config/JEPConfig.java
+++ src/main/java/xyz/wagyourtail/jsmacros/jep/config/JEPConfig.java
@@ -5,7 +5,7 @@ import xyz.wagyourtail.jsmacros.core.config.OptionType;
 import xyz.wagyourtail.jsmacros.jep.client.JsMacrosJEP;
 
 public class JEPConfig {
-    @Option(translationKey = "Shared Library Path", group = {"jsmacros.settings.languages", "jsmacros.settings.languages.jep"}, setter = "setPath", type = @OptionType(value = "file", options = "topLevel=MC"))
+    @Option(translationKey = "jsmacrosjep.sharedpath", group = {"jsmacros.settings.languages", "jsmacrosjep.settings.languages.jep"}, setter = "setPath", type = @OptionType(value = "file", options = "topLevel=MC"))
     public String path = "./jep.dll";
     
     public void setPath(String path) {
